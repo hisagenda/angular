48cbb041a5ea5edfcde5c703839a6ac2ab7e0067
Akinbade Abiola <abiola.kayode@gmail.com>
2019-10-18 16:24:13 +0100

    Enter

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,15 +4,17 @@ import {Component, Input} from '@angular/core'
     selector: 'event-thumbnail',
     template: `
         <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div>Time: {{event?.time}}</div>
             <div>Price: \${{event.price}}</div>
             <div>
-                <span>Location: {{event.location.address}}</span>
+                <span>Location: {{event?.location?.address}}</span>
                 <span> &nbsp; </span>
-                <span>Location: {{event.location.city}}, {{event.location.country}}</span>
+                <span>Location: {{event?.location?.city}}, {{event.location?.country}}</span>
             </div>
+            <div>
+            Online URL: {{event?.onlineUrl}}
         </div>
         `,
     styles: [`
